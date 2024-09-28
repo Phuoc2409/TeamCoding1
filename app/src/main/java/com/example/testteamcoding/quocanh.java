5f794678b738bc04f02c4662b3f460f13c6cfe67
BiBiOaiGi <tran.quocanh301003@gmail.com>
2024-09-28 21:31:07 +0700

    quocanh2

--- app/src/main/java/com/example/testteamcoding/quocanh.java
+++ app/src/main/java/com/example/testteamcoding/quocanh.java
@@ -19,6 +19,8 @@ public class quocanh extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets; //hh
+            //hahahahds
+            /kdkdkd
         });
     }
 }
